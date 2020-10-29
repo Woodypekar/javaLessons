6060da696e0ac1f302533439f364458d79408026
Nikita <Woodywoodpeker3@gmail.com>
2020-10-29 22:30:58 +0300

    Выполнено ДЗ номер 1

--- sandBox/src/main/java/ru/stqa/javaLessons/sandBox/MyFirstProgram.java
+++ sandBox/src/main/java/ru/stqa/javaLessons/sandBox/MyFirstProgram.java
@@ -4,6 +4,9 @@ public class MyFirstProgram {
 
     public static void main (String[] args) {
 
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(11, 22);
+        System.out.println(p1.distance(p2));
 
     }
 
