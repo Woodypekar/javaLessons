4de70b1f50f712778b156fb226ee3138515660e0
Nikita <Woodywoodpeker3@gmail.com>
2020-10-30 12:42:06 +0300

    Домашнее задание номер 2

---
+++ sandBox/src/test/java/ru/stqa/javaLessons/sandBox/pointTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.javaLessons.sandBox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class pointTests {
+
+    @Test
+    public void testDistance(){
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(2, 1);
+        int answer = (int) p1.distance(p2);
+        Assert.assertEquals(answer, 1);
+        Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+    }
+}
