6d7ff408455e956742b57080b4065a08e3bcdbfe
Woodypekar <Woodypekar@users.noreply.github.com>
2020-11-08 20:51:55 +0300

    Исправление названия пакета + класс на метод

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreatonsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreatonsTests.java
@@ -1,4 +1,4 @@
-package com.example.tests;
+package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
@@ -10,7 +10,7 @@ import ru.stqa.pft.addressbook.ContactData;
 public class ContactCreatonsTests {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -112,7 +112,7 @@ public class ContactCreatonsTests {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }
