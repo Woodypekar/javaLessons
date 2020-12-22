b9dea0b3f53244e755c11113b545162668ec16a0
Woodypekar <Woodypekar@users.noreply.github.com>
2020-12-22 16:21:12 +0300

    4.9 работает

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,14 +1,51 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+
+    private int idOfGroup;
     private final String nameOfGroup;
     private final String headerOfGroup;
     private final String footerOfGroup;
 
     public GroupData(String nameOfGroup, String headerOfGroup, String footerOfGroup) {
+        this.idOfGroup = 0;
         this.nameOfGroup = nameOfGroup;
         this.headerOfGroup = headerOfGroup;
         this.footerOfGroup = footerOfGroup;
+    }
+
+    public GroupData(int idOfGroup, String nameOfGroup, String headerOfGroup, String footerOfGroup) {
+        this.idOfGroup = idOfGroup;
+        this.nameOfGroup = nameOfGroup;
+        this.headerOfGroup = headerOfGroup;
+        this.footerOfGroup = footerOfGroup;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (idOfGroup != groupData.idOfGroup) return false;
+        return nameOfGroup != null ? nameOfGroup.equals(groupData.nameOfGroup) : groupData.nameOfGroup == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idOfGroup;
+        result = 31 * result + (nameOfGroup != null ? nameOfGroup.hashCode() : 0);
+        return result;
+    }
+
+    public void setIdOfGroup(int idOfGroup) {
+        this.idOfGroup = idOfGroup;
+    }
+
+    public int getIdOfGroup() {
+        return idOfGroup;
     }
 
     public String getNameOfGroup() {
@@ -21,5 +58,13 @@ public class GroupData {
 
     public String getFooterOfGroup() {
         return footerOfGroup;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "idOfGroup='" + idOfGroup + '\'' +
+                ", nameOfGroup='" + nameOfGroup + '\'' +
+                '}';
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationsTests.java
@@ -4,19 +4,28 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupCreationsTests extends TestBase {
 
 
   @Test
   public void testGroupCreations() {
-    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().goToGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().newGroup();
-    app.getGroupHelper().createNewGroup(new GroupData("Nikitosiki", null, null));
+    GroupData group = new GroupData("Nikitosiki2", null, null);
+    app.getGroupHelper().createNewGroup(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    group.setIdOfGroup(after.stream().max((o1, o2) -> Integer.compare(o1.getIdOfGroup(), o2.getIdOfGroup())).get().getIdOfGroup());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     app.logout();
   }
 }
